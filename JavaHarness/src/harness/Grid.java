929a4be680cd0e9ec28842f83ae2fcf72718b599
Jorge Figueroa <jorge.figueroa@ou.edu>
2013-11-19 20:38:53 -0600

    Created Grid class
    Grid will create the initial state of the world file

---
+++ JavaHarness/src/harness/Grid.java
@@ -0,0 +1,13 @@
+package harness;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Grid {
+	
+	private int width;
+	private int height;
+	private Set<Integer> aliveCells = new HashSet<Integer>();
+	
+	
+}
